1a7d38d4f43cdc7205dde5fce1d64d3f219491b1
kodovskii <61752634+kodovskii@users.noreply.github.com>
2020-05-02 08:24:54 +0200

    Starting with the front ...

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Konektiranje so Database
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper App' }));
 
@@ -10,4 +18,4 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));  
